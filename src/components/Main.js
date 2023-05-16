5069144809b5d848ef99fedc45e5f37dae04e494
dylanades <dylanad21@gmail.com>
2023-05-16 14:08:35 -0400

    deploy3

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ const Main = (props) => {
     const [ people, setPeople ] = useState(null)
 
     // const URL = "http://localhost:4000/people/"
-    const URL = "https://person-app-backend-vbps.onrender.com/people"
+    const URL = "https://person-app-backend-vbps.onrender.com/people/"
 
     const getPeople = async () => {
         const response = await fetch(URL)
