7eebdb34665f1cf8b0c1edf2d678214edc84f820
dylanades <dylanad21@gmail.com>
2023-05-23 08:30:50 -0400

    Ready to deploy

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,113 +1,112 @@
-import React from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Show = (props) => {
-  const { id } = useParams()
-  const navigate = useNavigate()
-  const dishes = props.dishes
-  
-  const dish = dishes ? dishes.find((p) => p._id === id ) : null
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const dishes = props.dishes;
 
-  const [ editForm, setEditForm ] = useState(dish)
+  const dish = dishes ? dishes.find((p) => p._id === id) : null;
 
-  const [ isEditing, setIsEditing ] = useState(false)
+  const [editForm, setEditForm] = useState(dish);
+  const [isEditing, setIsEditing] = useState(false);
 
-  useEffect( () => {
+  useEffect(() => {
     if (dish) {
-        setEditForm(dish)
+      setEditForm(dish);
     }
-  }, [dish])
+  }, [dish]);
 
-  // handling form data change
   const handleChange = (e) => {
-    setEditForm( {
+    setEditForm({
       ...editForm,
-     [e.target.name]: e.target.value 
-    })
-  }
-  
-  // handling submit event for edit form
-  const handleUpdate = (e) => {
-    e.preventDefault()
-    props.updateDishes(editForm, dish._id)
-  }
+      [e.target.name]: e.target.type === "checkbox" ? e.target.checked : e.target.value
+    });
+  };
 
-  // handle edit will toggle the edit form 
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    props.updateDishes(editForm, dish._id);
+  };
+
   const handleEdit = () => {
-    setIsEditing(prevState => !prevState)
-  }
+    setIsEditing((prevState) => !prevState);
+  };
 
   const handleDelete = () => {
-    props.deleteDishes(dish._id)
-    navigate('/')
-  }
+    props.deleteDishes(dish._id);
+    navigate('/');
+  };
 
   const loaded = () => {
-    const starRating = Math.min(dish.rating, 5); // Ensure the rating is capped at 5
-    const stars = "★".repeat(starRating) + "☆".repeat(5 - starRating); // Generate stars and empty stars
+    const starRating = Math.min(dish.rating, 5);
+    const stars = "★".repeat(starRating) + "☆".repeat(5 - starRating);
     const orderAgainText = dish.orderAgain ? "Would Order Again!" : "Would Not Order Again!";
     const orderAgainSymbol = dish.orderAgain ? "✓" : "✕";
-  
+
     return (
       <>
         <h1>{dish.name}</h1>
         <h2>{dish.restaurant}</h2>
         <h3>{stars}</h3>
         <h5>{orderAgainText} {orderAgainSymbol}</h5>
-        <img 
-          className="avatar-image" 
-          src={dish.image} 
-          alt={dish.name} 
+        <p>{dish.description}</p>
+        <img
+          className="avatar-image"
+          src={dish.image}
+          alt={dish.name}
           style={{ margin: "2vh auto", display: "block", maxWidth: "50vh" }}
         />
-        <button onClick={handleEdit}>{ isEditing ? 'Cancel Edit' : 'Edit' }</button>
+        <button onClick={handleEdit}>{isEditing ? 'Cancel Edit' : 'Edit'}</button>
         <button onClick={handleDelete}>Delete</button>
       </>
     );
   };
-  
-  
+
   const loading = () => {
-    return <h1>Loading ...</h1>;
+    return <h1>Loading...</h1>;
   };
 
   return (
     <div className="dish">
-      { dish ? loaded() : loading() }
+      {dish ? loaded() : loading()}
 
-
-      { isEditing && 
-      <form onSubmit={handleUpdate}>
-        <input
-          type="text"
-          value={editForm.name}
-          name="name"
-          placeholder="name"
-          onChange={handleChange}
-        /> 
-        <input
-          type="text"
-          value={editForm.image}
-          name="image"
-          placeholder="image URL"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={editForm.restaurant}
-          name="restaurant"
-          placeholder="restaurant"
-          onChange={handleChange}
-        />
-        <input
+      {isEditing && (
+        <form onSubmit={handleUpdate}>
+          <input
+            type="text"
+            value={editForm.name}
+            name="name"
+            placeholder="Name"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            value={editForm.image}
+            name="image"
+            placeholder="Image URL"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            value={editForm.restaurant}
+            name="restaurant"
+            placeholder="Restaurant"
+            onChange={handleChange}
+          />
+          <input
             type="number"
             value={editForm.rating}
             name="rating"
-            placeholder="rating"
+            placeholder="Rating"
             onChange={handleChange}
           />
-
+          <textarea id='textarea'
+            value={editForm.description}
+            name="description"
+            placeholder="Description"
+            onChange={handleChange}
+          />
           <label>
             <input
               type="checkbox"
@@ -117,11 +116,11 @@ const Show = (props) => {
             />
             Order Again
           </label>
-        <input type="submit" value="Update Dish" />
-      </form>
-        }
+          <input type="submit" value="Update Dish" />
+        </form>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Show
+export default Show;
